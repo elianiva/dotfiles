e6ac44a72378f681a8a937900ebbe3b669c6412c
elianiva <dicha.arkana03@gmail.com>
2020-10-17 22:24:31 +0700

    feat: truncate statusline if winwidth is lower than 80

--- nvim/.config/nvim/lua/modules/statusline.lua
+++ nvim/.config/nvim/lua/modules/statusline.lua
@@ -17,34 +17,45 @@ local colors = {
   line_col_alt  = '%#LineColAlt#',
 }
 
-local current_mode = function()
+local is_truncated = function()
+  local current_window = vim.fn.winnr()
+  local current_width = vim.fn.winwidth(current_window)
+  return current_width > 80
+end
+
+local get_current_mode = function()
   local modes = {
-    n      = 'Normal',
-    no     = 'N·Operator Pending',
-    v      = 'Visual',
-    V      = 'V·Line',
-    ['^V'] = 'V·Block',
-    s      = 'Select',
-    S      = 'S·Line',
-    ['^S'] = 'S·Block',
-    i      = 'Insert',
-    R      = 'Replace',
-    Rv     = 'V·Replace',
-    c      = 'Command',
-    cv     = 'Vim Ex ',
-    ce     = 'Ex ',
-    r      = 'Prompt ',
-    rm     = 'More ',
-    ['r?'] = 'Confirm ',
-    ['!']  = 'Shell ',
-    t      = 'Terminal '
+    n      = { 'Normal', 'N' };
+    no     = { 'N·Pending', 'N' };
+    v      = { 'Visual', 'V' };
+    V      = { 'V·Line', 'V' };
+    ['^V'] = {'V·Block', 'V'};
+    s      = {'Select', 'S'};
+    S      = {'S·Line', 'S'};
+    ['^S'] = {'S·Block', 'S'};
+    i      = {'Insert', 'S'};
+    R      = {'Replace', 'R'};
+    Rv     = {'V·Replace', 'V'};
+    c      = {'Command', 'C'};
+    cv     = {'Vim Ex ', 'V'};
+    ce     = {'Ex ', 'E'};
+    r      = {'Prompt ', 'P'};
+    rm     = {'More ', 'M'};
+    ['r?'] = {'Confirm ', 'C'};
+    ['!']  = {'Shell ', 'S'};
+    t      = {'Terminal ', 'T'};
   }
 
   local current_mode = vim.fn.mode()
-  return string.format(' %s ', modes[current_mode]):upper()
+
+  if is_truncated() then
+    return string.format(' %s ', modes[current_mode][1]):upper()
+  else
+    return string.format(' %s ', modes[current_mode][2]):upper()
+  end
 end
 
-local git_status = function()
+local get_git_status = function()
   local s = vim.call('sy#repo#get_stats')
   local branch = vim.call('fugitive#head')
 
@@ -58,7 +69,7 @@ local git_status = function()
   end
 end
 
-local filename = function()
+local get_filename = function()
   local filetype = vim.bo.filetype
   local filename = vim.fn.expand('%:t')
   local icon = require'nvim-web-devicons'.get_icon(
@@ -68,7 +79,7 @@ local filename = function()
   return string.format(' %s %s ', icon, filename)
 end
 
-local filetype = function()
+local get_filetype = function()
   local filetype = vim.bo.filetype
   if filetype == '' then
     return ''
@@ -77,19 +88,27 @@ local filetype = function()
   end
 end
 
+local get_line_col = function()
+  if is_truncated() then
+    return ' Ln %l, Col %c '
+  else
+    return ' %l:%c '
+  end
+end
+
 statusline = {}
 
 statusline.active = function()
   local mid = "%="
-  local mode = colors.mode .. current_mode()
+  local mode = colors.mode .. get_current_mode()
   local mode_alt = colors.mode_alt .. left_sep
-  local git = colors.git .. git_status()
+  local git = colors.git .. get_git_status()
   local git_alt = colors.git_alt .. left_sep
-  local filename = colors.filetype .. filename()
+  local filename = colors.filetype .. get_filename()
   local filename_alt = colors.filetype_alt .. right_sep
-  local filetype = colors.filetype .. filetype()
+  local filetype = colors.filetype .. get_filetype()
+  local line_col = colors.line_col .. get_line_col()
   local line_col_alt = colors.line_col_alt .. right_sep
-  local line_col = colors.line_col .. ' Ln %l, Col %c '
 
   return table.concat({
     colors.active,
