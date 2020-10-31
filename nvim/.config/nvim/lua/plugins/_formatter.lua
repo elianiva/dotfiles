4c5bbe58a2e81fded34d55742e6ef3ccd8c7d7bf
elianiva <dicha.arkana03@gmail.com>
2020-10-31 14:03:40 +0700

    chore: revert to Iosevka

--- nvim/.config/nvim/lua/plugins/_formatter.lua
+++ nvim/.config/nvim/lua/plugins/_formatter.lua
@@ -20,7 +20,7 @@ require'format'.setup {
 }
 remap('n', 'gf', '<cmd>FormatWrite<CR>', { noremap = true, silent = true })
 
-vim.cmd('augroup Format')
-vim.cmd('au!')
-vim.cmd('au BufWritePost * FormatWrite')
-vim.cmd('augroup END')
+-- vim.cmd('augroup Format')
+-- vim.cmd('au!')
+-- vim.cmd('au BufWritePost * FormatWrite')
+-- vim.cmd('augroup END')
