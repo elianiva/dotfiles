338fee485778a1e634903bf487b4b3ccfb585541
elianiva <dicha.arkana03@gmail.com>
2022-02-28 21:32:05 +0700

    remove lsp from nvim, add vscode settings

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,13 +1,13 @@
-require("modules.lsp")
+-- require("modules.lsp")
 
-require("rust-tools").setup {
-  tools = {
-    hover_actions = {
-      border = Util.borders,
-    },
-  },
-  server = {
-    standalone = false,
-    on_attach = Util.lsp_on_attach,
-  },
-}
+-- require("rust-tools").setup {
+--   tools = {
+--     hover_actions = {
+--       border = Util.borders,
+--     },
+--   },
+--   server = {
+--     standalone = false,
+--     on_attach = Util.lsp_on_attach,
+--   },
+-- }
